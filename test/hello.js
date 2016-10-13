81950c504b23df2b958fb6334d86a8bc3da2692c
Wes Eklund <wes.eklund@generationtux.com>
2016-10-13 14:19:06 -0400

    :recycle: cleanup work

--- test/hello.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var add = require("../app.js").add;
-
-describe("Hello World", function() {
-    it('should add 2 + 2', () => {
-        let result = add(2, 2);
-        assert.equal(result, 4);
-    });
-});
