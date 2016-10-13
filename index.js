371f330cd9b3d298360f9a81982d3b377fa9f583
Brandon Banks <b93banks@gmail.com>
2016-10-13 15:58:29 -0400

    Add create console command (#1)
    Add create console command

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-'use strict';
+#!/usr/bin/env node
 
 module.exports = function(number, locale){
     return number.toLocaleString(locale);
 };
+
+const program = require('commander');
+
+program
+    .version('0.0.1')
+    .command('create', 'Create object')
+    .parse(process.argv);
