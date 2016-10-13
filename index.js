a94989f8bf727a56aed2cbd7c113f1accfda4369
Brandon Banks <b93banks@gmail.com>
2016-10-13 15:05:05 -0400

    add create command file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
-'use strict';
 
 module.exports = function(number, locale){
     return number.toLocaleString(locale);
 };
 
-console.log('Hello, world!');
+const program = require('commander');
+
+program
+    .version('0.0.1')
+    .command('create', 'Create object')
+    .parse(process.argv);
