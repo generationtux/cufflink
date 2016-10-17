7577c29f67d742be7e0bbd722892e1f7e7bf48fe
Brandon Banks <b93banks@gmail.com>
2016-10-17 11:19:22 -0400

    remove num formatter title

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 var expect = require('chai').expect;
 var numFormatter = require('../index');
 
-describe('#numFormatter', () => {
+describe('example test', () => {
     it('should add 2 + 2', () => {
         var result = 2 + 2;
         expect(result).to.equal(4);
