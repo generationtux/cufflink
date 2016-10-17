ab69202eab64bc977b8c385ecb3643f106671375
Wes Eklund <wes.eklund@generationtux.com>
2016-10-17 14:43:04 -0400

    :recycle: additional cmd refactor

--- src/cufflink.js
+++ src/cufflink.js
@@ -2,14 +2,16 @@
 
 process.env.FORCE_COLOR = true;
 
-const program = require('commander');
+const program = require('../node_modules/commander');
 const package = require('../package.json');
 
 program
     .version((package.name) + '@' + (package.version))
     .description('A command tool to help generate seed data based on your object schema in your applications')
     .command('create', 'Generate seed data')
+
+
+
     .command('teardown', 'Destroy your seeded objected with your drivers used')
     .command('drivers', 'List currently created drivers')
     .parse(process.argv);
-
