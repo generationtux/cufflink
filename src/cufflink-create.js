b65e567881d4af197c3411e7c59b1e742a6d967f
Wes Eklund <wes.eklund@generationtux.com>
2016-10-18 10:08:06 -0400

    working

--- src/cufflink-create.js
+++ src/cufflink-create.js
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-
 const create = function() {};
 
-function buildDependencyTree(){
-    var graph = {};
-
-    return graph;
-}
-
-function getDrivers(){
-
-}
-
-function getObjects(){
-
-}
-
-create.prototype.setup = function(objectName, cb) {
-    var graph = buildDependencyTree();
-
-}
-
-create.prototype.seedObject = function(cb){
-
-}
+console.log('hello');
