5f97611fa67ef76fbe8148b7dfc60e552280e90d
Troy Harvey <troy.harvey@seven-26.com>
2016-10-20 09:39:50 -0400

    TearDown command using promises

--- src/TearDownCommand.js
+++ src/TearDownCommand.js
@@ -3,11 +3,24 @@ class TearDownCommand {
         this.pathToJsonFile = pathToJsonFile;
         this.driverLocator = driverLocator;
         this.fs = fs;
+        this.driversToExecute = [];
+    }
+
+    runRecursive() {
+        if (this.driversToExecute.length == 0){
+            return new Promise((resolve) => {
+                resolve();
+            });
+        }
+
+        return this.driversToExecute[0].tearDown().then((results) => {
+            this.driversToExecute.shift();
+            this.runRecursive();
+        });
     }
 
     run() {
         let seededData = this.fs.readFileSync(this.pathToJsonFile);
-
         seededData.reverse();
 
         let drivers = this.driverLocator.drivers();
@@ -16,13 +29,14 @@ class TearDownCommand {
             let type = row["type"];
             if(type in drivers){
                 let driver = drivers[type];
-                driver.tearDown();
+                this.driversToExecute.push(driver);
             }else{
                 throw Error("Seeded data file does not contain a type that you have a driver for");
             }
         });
 
+        return this.runRecursive();
     }
 }
 
-module.exports.TearDownCommand = TearDownCommand;
+module.exports = TearDownCommand;
